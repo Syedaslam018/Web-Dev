fe66650ecbc13a175eadfaf52b501aaed06660d1
Aslam <71985510+Syedaslam018@users.noreply.github.com>
2023-05-11 07:53:16 -0700

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,8 @@
+let titleHead = document.getElementById("header-title");
+titleHead.textContent = "Namaste";
+let head = document.getElementById("main-header");
+head.style.borderBottom = '3px solid black'
+let addItems = document.getElementsByClassName("title");
+//console.log(addItems);
+addItems[0].style.fontWeight = 'bold';
+addItems[0].style.color = 'green';
