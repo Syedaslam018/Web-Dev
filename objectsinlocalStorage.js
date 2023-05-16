69a7dff7795beb278094d0e909064dc327ff7087
Aslam <71985510+Syedaslam018@users.noreply.github.com>
2023-05-16 14:47:57 +0530

    Update objectsinlocalStorage.js

--- objectsinlocalStorage.js
+++ objectsinlocalStorage.js
@@ -1,19 +1,39 @@
 //Now try storing the user details as an object in the local storage
 
-myForm.addEventListener('submit', onSubmit);
+let myForm = document.getElementById('myForm');
+let ul = document.getElementById('users');
 
-function onSubmit(e) {
+//event listener
+myForm.addEventListener('submit',onSubmit);
+
+//empty array for storing arrays
+let listUsers = [];
+
+//action event function
+function onSubmit(e){
+    //adding data to localStroage
+    let ipname = document.getElementById('name').value;
+    let ipmail = document.getElementById('email').value;
     e.preventDefault();
     let myObj = {
-        name: iname.value,
-        email: imail.value
+        fname: ipname,
+        fmail: ipmail
     };
+localStorage.setItem('users', JSON.stringify(myObj));
 
-    myObj_converted = JSON.stringify(myObj);
-    
-    localStorage.setItem('myObj', myObj_converted);
-
-
-
-    myObj_normalised = JSON.parse(myObj_converted);
+//adding data to website
+let li = document.createElement('li');
+li.innerHTML = ipname + ' ' + ipmail;
+let butt = document.createElement('input');
+butt.type = 'button';
+butt.value = 'delete';
+butt.className = btn;
+butt.id = 'delete';
+butt.onclick = function(){
+    ul.removeChild(li);
+    localStorage.removeItem('users');
+};
+li.appendChild(butt);
+ul.appendChild(li);
+document.myForm.reset();
 }
